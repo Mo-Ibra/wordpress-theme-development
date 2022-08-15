a0793757fcf861c97afc7c06457db948a5ddd2c1
Dev-Maradona <maradona.0000@yahoo.com>
2022-08-15 16:08:28 -0700

    support features with wp_theme_support.

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Bootstraps the Theme.
  *
@@ -9,11 +10,13 @@ namespace AQUILA_THEME\Inc;
 
 use AQUILA_THEME\Inc\Traits\Singleton;
 
-class AQUILA_THEME {
-	
+class AQUILA_THEME
+{
+
 	use Singleton;
 
-	protected function __construct() {
+	protected function __construct()
+	{
 
 		Assets::get_instance();
 
@@ -21,7 +24,63 @@ class AQUILA_THEME {
 		$this->set_hooks();
 	}
 
-	protected function set_hooks() {
-		// actions and filters
+	protected function set_hooks()
+	{
+		/**
+		 * Actions
+		 */
+		add_action('after_setup_theme', [$this, 'setup_theme']);
+	}
+
+	public function setup_theme()
+	{
+
+		// Add Title
+		add_theme_support('title-tag');
+
+		// Add Custom Logo
+		add_theme_support('custom-logo', [
+			'header-text' => ['site-title', 'site-description'],
+			'height' => 100,
+			'width' => 400,
+			'flex-height' => true,
+			'flex-width' => true,
+		]);
+
+		// Add Custom Background
+		add_theme_support('custom-background', [
+			'background-color' => '#fff',
+			'default-image' => '',
+			'default-repeat' => 'no-repeat',
+		]);
+
+		// Post Thumbnail
+		add_theme_support('post-thumbnails');
+
+		// Enables Selective Refresh for Widgets
+		add_theme_support('customize-selective-refresh-widgets');
+
+		// Add feed links in feed tag.
+		add_theme_support('automatic-feed-links');
+
+		// Allows the use of HTML5 markup
+		add_theme_support('html5', [
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		]);
+
+		add_editor_style();
+		add_theme_support('wp-block-styles');
+
+		global $content_width;
+
+		if (!isset($content_width)) {
+			$content_width = 1920;
+		}
 	}
 }
